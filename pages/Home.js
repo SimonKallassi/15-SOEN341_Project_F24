c04cd3b9928d62776b8a568553779a5a90b8f399
Simon Kallassi <98189147+SimonKallassi@users.noreply.github.com>
2024-09-20 17:33:55 -0400

    first commit

---
+++ pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <main style={{ padding: '20px' }}>
+                <section>
+                    <h1>Welcome to PeerEvaluator</h1>
+                    <p>To help evaluate your peers .</p>
+                </section>
+                <section>
+                    <h2>Teacher and Students</h2>
+                </section>
+            </main>
+        </div>
+    );
+};
+
+export default Home;
