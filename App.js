26ebee5b396f8d9fcd539c8fe036766d05ce0f82
Paoleno Nikyema <paoleno.nikyema@mail.concordia.ca>
2024-09-22 23:39:00 -0400

    update

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import loginPage from './src/loginPage';
+import loginPage from './pages/loginPage';
 const App = () => {
     return (
         <Router>
