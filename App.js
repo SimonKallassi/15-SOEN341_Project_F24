2394ccb00645667a3cb90d2236ea8521dda69d19
bardobardo21 <youssefmarzouk2112@gmail.com>
2024-09-23 13:40:45 -0400

    Added a missing slash to the import

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import LoginPage from './pages/Login';
-import SignUpPage from '.pages/SignUp';
+import SignUpPage from './pages/SignUp';
 
 const App = () => {
     return (
