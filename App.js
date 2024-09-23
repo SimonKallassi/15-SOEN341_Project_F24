94d1d891d6621393447e36a07a9a24ffc7aedb1d
bardobardo21 <youssefmarzouk2112@gmail.com>
2024-09-22 20:53:30 -0400

    Created a Sign Up Page

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import LoginPage from './pages/Login';
+import SignUpPage from '.pages/SignUp';
+
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/LoginPage" element={<LoginPage />} />
+                <Route path="/SignUpPage" element={<SignUpPage />} />
             </Routes>
         </Router>
     );
