510be7f9f7aff745386b8e54609df6e6a59f0b53
Paoleno Nikyema <paoleno.nikyema@mail.concordia.ca>
2024-09-23 16:00:02 -0400

    added more styles and updated Login.js

--- pages/Login.js
+++ pages/Login.js
@@ -4,40 +4,48 @@ import '../styles/Login.css';
 
 const Login = () => {
     const navigate = useNavigate();
-   // function SubmitButton() {
-        const handleSubmit = (event) => {
-          
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        //check that the input is not blank
+        if (!email || !password) {
+            alert('Please fill in all fields.');
+            return;
+          }
           alert('Submit clicked!');
         };
-   // }
+        
     return (
         <div class="container">
             <div class="header">
                Sign In
             </div>
-
-            <div class="inputs">
+            <form onSubmit={handleSubmit}>
                 <div class="inputs">
-                   
-                    <div class="input" id="username" >
-                        <img src ="../icons/username.png" />
-                        <input type="text" placeholder="Username"/>
+                    <div class="inputs">
+                    
+                        <div class="input" id="username" >
+                            {/* <img src ="../icons/username.png" /> */}
+                            <input type="text" placeholder="Username" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        </div>
+                        <div class="input" id="password" >
+                            {/* <img src ="../icons/password.svg" /> */}
+                            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        </div>
                     </div>
-                    <div class="input" id="password" >
-                        <img src ="../icons/password.svg" />
-                        <input type="password" placeholder="Password"/>
+
+                    <div class="buttons">
+                        <button type="submit" id="submit">Login</button>
+                        {/* <div onClick={handleSubmit} class="button" id="submit">LogIn</div> */}
+                        {/* Used html link <a></a> for now just for test. Will change using native react routers when pages are created */}
+                        <div class="link" id=""><a href ="#">Lost Password?</a> </div>
+                        <div class="link" id=""><a href ="#">Lost username?</a> </div>
+                        <div id="">Not registered yet? <span class="link"><a href ="#">Sign up</a></span> </div>
                     </div>
                 </div>
-
-                <div class="buttons">
-                    <div onClick={handleSubmit} class="button" id="submit">Log In</div>
-                    {/* Used html link <a></a> for now just for test. Will change using native react routers when pages are created */}
-                    <div class="link" id=""><a href ="#">Lost Password?</a> </div>
-                    <div class="link" id=""><a href ="#">Lost username?</a> </div>
-                    <div id="">Not registered yet? <span class="link"><a href ="#">Sign up</a></span> </div>
-                </div>
-            </div>
-
+            </form>
         </div>
     );
 };
