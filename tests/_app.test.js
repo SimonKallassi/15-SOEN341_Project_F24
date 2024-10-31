f3fe3f449d6397ed97e01bb40310162ebaf53bb0
NNazary <najmuddin000@gmail.com>
2024-10-31 02:48:46 -0400

    creating test function inside app.test.js to render the the headers

--- tests/_app.test.js
+++ tests/_app.test.js
@@ -2,3 +2,6 @@ import { render, screen } from '@testing-library/react';
 import MyApp from '../pages/_app';
 import '@testing-library/jest-dom';
 
+test("Renders header with navigation links", () => {
+    
+});
