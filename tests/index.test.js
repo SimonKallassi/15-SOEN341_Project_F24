fb026f3df52af304f870ac34d01d55be91edf888
NNazary <najmuddin000@gmail.com>
2024-10-31 03:10:41 -0400

    created 2 unit test inside index.test.js
    - Test that the welcome message heading is displayed with the correct text.
    - Verify the presence of the 'Get Started' button and check that it links to the Login page.

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,5 +3,12 @@ import Home from '../pages/index';
 import '@testing-library/jest-dom';
 
 test("Displays welcome message", () => {
+    render(<Home />);
+    const welcomeElement = screen.getByRole('heading', { level: 1, name: /Welcome to/i });
+    expect(welcomeElement).toHaveTextContent("Welcome to PeerEvaluator");
+});
 
+test("Has Get Started button linking to Login", () => {
+    render(<Home />);
+    expect(screen.getByText("Get Started").closest('a')).toHaveAttribute('href', '/Login');
 });
