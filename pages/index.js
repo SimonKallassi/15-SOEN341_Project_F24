7779950a56ca6df4381593c9288879f3733483e8
bardobardo21 <youssefmarzouk2112@gmail.com>
2024-11-10 18:14:38 -0500

    Changed the design of the Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,20 @@
 import Link from 'next/link';
 
 const Home = () => {
-
     return (
         <div className="split">
-            <div className="left">
                 <main style={{ padding: '20px' }}>
                     <section>
-                        <h1 class="title">Welcome to <br></br><span id="name"> PeerEvaluator</span></h1>
-                        <h3 class="slogan">To help evaluate your peers</h3>
+                        <h1 className="title">Welcome to <br /><span id="name">PeerEvaluator</span></h1>
+                        <h3 className="slogan">Helping you evaluate your peers effectively</h3>
                     </section>
                     <section className="buttons">
                         <Link href="/Login">
-                            <button id="start"> <span>Get Started</span></button>
+                            <button id="start"><span>Get Started</span></button>
                         </Link>
                     </section>
-                </main>
-            </div>
-            <div className="right">
-                <img className="home-image" src="https://images.vexels.com/media/users/3/139257/isolated/preview/b8fa8f291632f8fe68842e4fb100d8e0-square-rectangle-shape.png" alt="image placeholder"/>
-            </div>
+                </main>     
         </div>
-        
     );
 };
 
