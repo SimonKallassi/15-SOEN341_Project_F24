ba909f4a8dcaa6da06cff6c96494c6c07ea251ea
NNazary <124314406+NNazary@users.noreply.github.com>
2024-11-14 01:10:38 -0500

    Update Login.test.js

--- tests/frontend/Login.test.js
+++ tests/frontend/Login.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import Login from '../pages/Login';
+import Login from '../../pages/Login';
 import '@testing-library/jest-dom';
 jest.spyOn(window, 'alert').mockImplementation(() => {});
 
