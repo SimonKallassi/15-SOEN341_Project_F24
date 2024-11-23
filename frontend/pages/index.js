7d93ed8f3c5782d60a09e22451dba5359f30b909
bardobardo21 <youssefmarzouk2112@gmail.com>
2024-11-23 12:59:01 -0500

    fixed UI of Home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,17 +3,19 @@ import Link from 'next/link';
 const Home = () => {
     return (
         <div className="split">
-                <main style={{ padding: '20px' }}>
-                    <section>
-                        <h1 className="title">Welcome to <br /><span id="name">PeerEvaluator</span></h1>
-                        <h3 className="slogan">Helping you evaluate your peers effectively</h3>
-                    </section>
-                    <section className="buttons">
-                        <Link href="/Login">
-                            <button id="start"><span>Get Started</span></button>
-                        </Link>
-                    </section>
-                </main>     
+            <main>
+                <section>
+                    <h1 className="title">
+                        Welcome to <br/><span id="name">PeerEvaluator</span>
+                    </h1>
+                    <h3 className="slogan">Helping you evaluate your peers effectively</h3>
+                </section>
+                <section>
+                    <Link href="/Login">
+                        <button id="start"><span>Get Started</span></button>
+                    </Link>
+                </section>
+            </main>
         </div>
     );
 };
